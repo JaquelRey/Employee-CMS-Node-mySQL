59a2243acf0abfff6626695cce2bb38f5ed751a6
ericchase <github@eric.addymail.com>
2022-10-23 03:03:33 -0500

    refactor: minimize / comment unnecessary code
    Signed-off-by: ericchase <github@eric.addymail.com>

--- routes/index.js
+++ routes/index.js
@@ -25,34 +25,22 @@ function quit() {
 
 function dispatch(query, controllers) {
     switch (query.choice) {
+        //case COMMANDS.DEPARTMENTS_ADD: break;
+        //case COMMANDS.DEPARTMENTS_REMOVE: break;
+        //case COMMANDS.DEPARTMENTS_VIEW_SALARIES: break;
+        //case COMMANDS.DEPARTMENTS_VIEW: break;
+        //case COMMANDS.EMPLOYEES_ADD: break;
+        //case COMMANDS.EMPLOYEES_REMOVE: break;
+        //case COMMANDS.EMPLOYEES_UPDATE_MANAGER: break;
+        //case COMMANDS.EMPLOYEES_UPDATE_ROLE: break;value
+        //case COMMANDS.EMPLOYEES_VIEW_BY_DEPARTMENT: break;
+        //case COMMANDS.EMPLOYEES_VIEW_BY_MANAGER: break;
         case COMMANDS.EMPLOYEES_VIEW:
+
             break;
-        // case COMMANDS.EMPLOYEES_VIEW_BY_DEPARTMENT:
-        //     break;
-        // case COMMANDS.EMPLOYEES_VIEW_BY_MANAGER:
-        //     break;
-        // case COMMANDS.EMPLOYEES_ADD:
-        //     break;
-        // case COMMANDS.EMPLOYEES_REMOVE:
-        //     break;
-        // case COMMANDS.EMPLOYEES_UPDATE_ROLE:
-        //     break;
-        // case COMMANDS.EMPLOYEES_UPDATE_MANAGER:
-        //     break;
-        // case COMMANDS.ROLES_VIEW:
-        //     break;
-        // case COMMANDS.ROLES_ADD:
-        //     break;
-        // case COMMANDS.ROLES_REMOVE:
-        //     break;
-        // case COMMANDS.DEPARTMENTS_VIEW:
-        //     break;
-        // case COMMANDS.DEPARTMENTS_ADD:
-        //     break;
-        // case COMMANDS.DEPARTMENTS_REMOVE:
-        //     break;
-        // case COMMANDS.DEPARTMENTS_VIEW_SALARIES:
-        //     break;
+        //case COMMANDS.ROLES_ADD: break;
+        //case COMMANDS.ROLES_REMOVE: break;
+        //case COMMANDS.ROLES_VIEW: break;
         case COMMANDS.QUIT:
         default:
             return quit();
@@ -67,66 +55,22 @@ module.exports = function ({ controllers, utils }) {
         name: "choice",
         message: "What would you like to do?",
         choices: [
-            {
-                name: "View employees",
-                value: COMMANDS.EMPLOYEES_VIEW,
-            },
-            // {
-            //     name: "View employees by department",
-            //     value: COMMANDS.EMPLOYEES_VIEW_BY_DEPARTMENT,
-            // },
-            // {
-            //     name: "View employees by manager",
-            //     value: COMMANDS.EMPLOYEES_VIEW_BY_MANAGER,
-            // },
-            // {
-            //     name: "Add employee",
-            //     value: COMMANDS.EMPLOYEES_ADD,
-            // },
-            // {
-            //     name: "Remove employee",
-            //     value: COMMANDS.DEPARTMENTS_REMOVE,
-            // },
-            // {
-            //     name: "Update Employee Role",
-            //     value: COMMANDS.EMPLOYEES_UPDATE_ROLE,
-            // },
-            // {
-            //     name: "Update Employee Manager",
-            //     value: COMMANDS.EMPLOYEES_UPDATE_MANAGER,
-            // },
-            // {
-            //     name: "View All Roles",
-            //     value: COMMANDS.ROLES_VIEW,
-            // },
-            // {
-            //     name: "Add Role",
-            //     value: COMMANDS.ROLES_ADD,
-            // },
-            // {
-            //     name: "Remove Role",
-            //     value: COMMANDS.ROLES_REMOVE,
-            // },
-            // {
-            //     name: "View All Departments",
-            //     value: COMMANDS.DEPARTMENTS_VIEW,
-            // },
-            // {
-            //     name: "Add Department",
-            //     value: COMMANDS.DEPARTMENTS_ADD,
-            // },
-            // {
-            //     name: "Remove Department",
-            //     value: COMMANDS.DEPARTMENTS_REMOVE,
-            // },
-            // {
-            //     name: "View Total Utilized Budget By Department",
-            //     value: COMMANDS.DEPARTMENTS_VIEW_SALARIES,
-            // },
-            {
-                name: "Quit",
-                value: COMMANDS.QUIT,
-            },
+            //{ value: COMMANDS.DEPARTMENTS_ADD,              /**/ name: "Add Department"                           /**/, },
+            //{ value: COMMANDS.DEPARTMENTS_REMOVE,           /**/ name: "Remove Department"                        /**/, },
+            //{ value: COMMANDS.DEPARTMENTS_REMOVE,           /**/ name: "Remove employee"                          /**/, },
+            //{ value: COMMANDS.DEPARTMENTS_VIEW_SALARIES,    /**/ name: "View Total Utilized Budget By Department" /**/, },
+            //{ value: COMMANDS.DEPARTMENTS_VIEW,             /**/ name: "View All Departments"                     /**/, },
+            //{ value: COMMANDS.EMPLOYEES_ADD,                /**/ name: "Add employee"                             /**/, },
+            //{ value: COMMANDS.EMPLOYEES_UPDATE_MANAGER,     /**/ name: "Update Employee Manager"                  /**/, },
+            //{ value: COMMANDS.EMPLOYEES_UPDATE_ROLE,        /**/ name: "Update Employee Role"                     /**/, },
+            //{ value: COMMANDS.EMPLOYEES_VIEW_BY_DEPARTMENT, /**/ name: "View employees by department"             /**/, },
+            //{ value: COMMANDS.EMPLOYEES_VIEW_BY_MANAGER,    /**/ name: "View employees by manager"                /**/, },
+            { value: COMMANDS.EMPLOYEES_VIEW,               /**/ name: "View employees"                           /**/, },
+            //{ value: COMMANDS.ROLES_ADD,                    /**/ name: "Add Role"                                 /**/, },
+            //{ value: COMMANDS.ROLES_REMOVE,                 /**/ name: "Remove Role"                              /**/, },
+            //{ value: COMMANDS.ROLES_VIEW,                   /**/ name: "View All Roles"                           /**/, },
+            { value: COMMANDS.QUIT,                         /**/ name: "Quit"                                     /**/, },
+
         ],
     });
 
