137e4cf737a522fedac6483aac25cbd05e1bfd5d
jaquel <jaquelrey@gmail.com>
2022-11-06 17:14:04 -0500

    resolving missing file

--- db/connections/rolefuncs.js
+++ db/connections/rolefuncs.js
@@ -1,20 +1,27 @@
 const connection = require("../connection");
-const DB = require("./database");
 
-class RoleDb extends DB {
+class RoleDb {
+  constructor(connection) {
+    this.connection = connection;
+  }
+
   // role functions with mysql2 promise function
   // create role, remove role, view roles
 
   addRoles(role) {
-    return connection.query("INSERT INTO role SET ?", role);
+    return this.connection.promise().query("INSERT INTO role SET ?", role);
   }
 
   removeRoles(roleId) {
-    return connection.query("DELETE FROM role WHERE id = ?", roleId);
+    return this.connection
+      .promise()
+      .query("DELETE FROM role WHERE id = ?", roleId);
   }
 
   viewAllRoles() {
-    return connection.promise().query(
+    return this.connection
+      .promise()
+      .query(
         "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
       );
   }
